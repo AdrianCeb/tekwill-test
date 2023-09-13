acba1f1f288ee81e155b69e922ea9d8a58a9537a
Professional3 <adce74@gamail.com>
2023-09-13 17:24:49 +0300

    homework_nr_2

---
+++ src/main/java/homework_nr_2/HomeworkNr2.java
@@ -0,0 +1,51 @@
+package homework_nr_2;
+
+public class HomeworkNr2 {
+
+    public static void main(String[] args) {
+        System.out.println(" Hello World!");
+
+
+
+       long CurrentWorldPopulation = 7811080815L;
+       int CarsProducedThisYear = 55235358;
+
+        char FirstLetterInAlphabet = 'A';
+
+        boolean JavasAmazing = true;
+
+        float Pi = 3.14159F;
+
+        System.out.println( CurrentWorldPopulation );
+        System.out.println( CarsProducedThisYear );
+        System.out.println( FirstLetterInAlphabet );
+        System.out.println( JavasAmazing );
+        System.out.println( Pi );
+
+
+
+        // p.6 Определите и инициализируйте следующие переменные
+
+        double Price = 123456789.123456789;
+        double Tax = 123456789.123456789;
+        int Quantity = 10;
+        double Sum = 0;
+
+        System.out.println( Price );
+        System.out.println( Tax );
+        System.out.println( Quantity );
+        System.out.println( Sum );
+
+        // Определите переменную total по следующей формуле:
+        // total = price + tax * quantity + sum и выведите это значение на экран
+
+
+        double total = Price + Tax * Quantity + Sum;
+
+        System.out.println( total );
+
+
+
+    }
+
+}
