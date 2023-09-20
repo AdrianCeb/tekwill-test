19eccefcc4bfec9a349923a49c4c7c26c033b30f
Professional3 <adce74@gamail.com>
2023-09-20 20:13:19 +0300

    homework_nr_3_2

---
+++ src/main/java/homework_nr_3/HomeworkNr3_2.java
@@ -0,0 +1,11 @@
+package homework_nr_3;
+
+public class HomeworkNr3_2 {
+
+    public static void main(String[] args) {
+
+        for ( int i = 100; i <= 1000; i +=5 ) {
+            System.out.println(i);
+        }
+    }
+}
