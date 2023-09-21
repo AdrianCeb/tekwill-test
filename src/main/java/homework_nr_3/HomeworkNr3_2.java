d16ee8d19d3c139990020d4fae2169c8132ea272
Professional3 <adce74@gamail.com>
2023-09-21 11:26:11 +0300

    homework_nr_2 new

--- src/main/java/homework_nr_3/HomeworkNr3_2.java
+++ src/main/java/homework_nr_3/HomeworkNr3_2.java
@@ -2,7 +2,11 @@ package homework_nr_3;
 
 public class HomeworkNr3_2 {
 
-    public static void main(String[] args) {
+    // Внутри метода main напишите цикл который отображает в консоль
+    // все числа от 100 до 1000 которые делятся на 5
+
+
+        public static void main(String[] args) {
 
         for ( int i = 100; i <= 1000; i +=5 ) {
             System.out.println(i);
